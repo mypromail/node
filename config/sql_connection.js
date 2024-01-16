ba1fc5c622ac76da9353877da9b5edc8b4db140e
GABRIEL SGARIA BERNARDINO <gabriel_bernardino1@estudante.sesisenai.org.br>
2024-01-16 14:45:58 -0300

    Criação de tabela produto

---
+++ config/sql_connection.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('node_project', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = sequelize;
---
+++ models/Produto.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/sql_connection');
+
+class Product extends Model {}
+
+Product.init({
+    id_product: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name_product: {
+        type: DataTypes.STRING
+    },
+    descricao: {
+        type: DataTypes.TEXT
+    },
+    preco_unitario: {
+        type: DataTypes.DECIMAL
+    }
+}, {
+    sequelize,
+    modelsName: 'product',
+    timestamps: false
+});
+
+module.exports = Product;
