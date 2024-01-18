115ba263e4b31de549dadcb847b6c6d0c2cd38f1
GABRIEL SGARIA BERNARDINO <gabriel_bernardino1@estudante.sesisenai.org.br>
2024-01-18 13:57:43 -0300

    Criação de product_routes

---
+++ routes/product_routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
+
+router.post('/produtos', productController.createProduct);
+
+router.get('/produtos', productController.getAllProducts);
+
+router.get('/produtos/:id', productController. getProductById);
+
+router.get('/produtos/:id', productController.updateProduct);
+
+router.delete('/produtos/:id', productController.deleteProduct);
+
+router.post('/produtos/:id/entrada', productController.registerEntry);
+
+router.post('/produtos/:id/saida', productController.registerExit);
+
+module.exports = router;
